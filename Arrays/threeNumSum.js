46f78b028703f27f768ddc2043b66c1a7ed3546a
Cstopyak <69408482+Cstopyak@users.noreply.github.com>
2021-06-09 09:36:18 -0500

    3sum

--- Arrays/threeNumSum.js
+++ Arrays/threeNumSum.js
@@ -24,32 +24,64 @@
 //two pointers should equal up to our target
 //check to see if both pointers add up to target
 
+const threeSum = nums => {
+    nums.sort((a, b) => a - b);
+    let res = []; //new array for target value
+    for (let i = 0; i < nums.length; i++) {
+        const target = 0 - nums[i];
+        let left = i + 1;
+        let right = nums.length - 1;
 
+        //duplicates
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        while (left < right) {
+            if (nums[left] + nums[right] === target) {
+                res.push([nums[i], nums[left], nums[right]]);
+                left++;
+                while (nums[left] === nums[left - 1]) { //keeps incrementing pass duplicates
+                    left++;
+                }
+            } else if (nums[left] + nums[right] < target) { //if they dont meet the target
+                left++
+            } else {
+                right--
+            }
+        }
+
+
+    }
+    return res;
+};
+
+//option 2 not fixed runs infin loop
 function threeSum(array, targetSum) {
     array.sort((a, b) => a - b);
     let triples = []; //new array for target value
-    
+
     for (let i = 0; i < array.length - 2; i++) {
         let left = i + 1;
         let right = array.length - 1;
-        
-        while(left < right){
+
+        while (left < right) {
 
             const currSum = array[i] + array[left] + array[right];
 
-            if(currSum === targetSum){
+            if (currSum === targetSum) {
                 triples.push([array[i], array[left] + array[right]]);
-                left ++;
-                right --;
-            }else if( currSum < targetSum){
-                left ++;
-            }else if( currSum > targetSum){
-                right --;
+                left++;
+                right--;
+            } else if (currSum < targetSum) {
+                left++;
+            } else if (currSum > targetSum) {
+                right--;
             }
         }
     }
     return triples;
 };
 
-console.log(threeSum([-1, 0, 1, 2, -1, -4], [-1,-1,2],[-1,0,1]));
+console.log(threeSum([-1, 0, 1, 2, -1, -4], [-1, -1, 2], [-1, 0, 1]));
 
