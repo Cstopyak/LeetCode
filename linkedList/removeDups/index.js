e6149a6fe989aa1f238630afb96f33d026a71c6f
Cstopyak <69408482+Cstopyak@users.noreply.github.com>
2021-06-18 18:31:17 -0500

    psuedo code

---
+++ linkedList/removeDups/index.js
@@ -0,0 +1,24 @@
+// questions to ask
+
+//What do we notice about the question
+//linked list are a lot like arrays, we have to loop through
+
+//What are we being asked to do?
+//to identify the all the dups and remove them or skip over
+// output 1 -> 3-> 4-> 5-> 6
+
+//Whats the information thats given
+//list is already sorted
+// we have 1 -> 1-> 3-> 4-> 4-> 5-> 6-> 6
+
+//how can we manipulate or what do we need
+//we need identify current node (head)
+// we will need to be able to store currentnode as a temp so we dont loose it
+//need to identify next value in the linked list that is distinct
+//we will need something to track where we currently are
+
+
+
+
+// given X 1 -> 1-> 3-> 4-> 4-> 5-> 6-> 6
+
