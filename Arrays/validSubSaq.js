2ebf09eaa7096956268b904527fc2c146d7aadf1
Cstopyak <69408482+Cstopyak@users.noreply.github.com>
2021-06-23 14:48:39 -0500

    valid Sub

---
+++ Arrays/validSubSaq.js
@@ -0,0 +1,18 @@
+
+
+array= [5,1,22,25,6,-1,8,10];
+sequence = [1,6,-1,10]
+
+function isValidSubsequence(array, sequence) {
+    // Write your code here.
+    arrayIndex = 0;
+    seqIndex = 0;
+    while (arrayIndex < array.length && seqIndex < sequence.length) {
+        if (array[arrayIndex] === sequence[seqIndex])
+            seqIndex++;
+        arrayIndex++;
+    }
+    return seqIndex === sequence.length;
+}
+
+console.log(isValidSubsequence(array, sequence));
