5d8c872e29e4d1e1565defb4a28ed5357416ae26
Cstopyak <69408482+Cstopyak@users.noreply.github.com>
2021-07-14 16:19:35 -0500

    string caesar

---
+++ Strings/caesarCipherEncryptor.js
@@ -0,0 +1,11 @@
+//given a non-empty string of lowercase letters and a non neg integer representing a key, write the function that returns a new string obtained by shifting every letter in the input string by k positions in the alphabet, where k is the key
+
+
+
+
+// inputs
+// string = "xyz"
+//key = 2
+
+//output
+// "zab"
